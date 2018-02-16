f5bea951ad50d1b0d65d8b0f65f05586d6a3f6ac
Patrick Blomquist <PABL@statoil.com>
2018-02-16 10:59:00 +0100

    The first project commit

---
+++ main.R
@@ -0,0 +1,29 @@
+### Load libraries ###############
+
+library(dplyr)
+library(stringr)
+library(tm)
+
+### Load data################
+
+df_train <- read.csv("data/train.csv", sep = ",", stringsAsFactors = F)
+
+
+####### Text specifics ###############
+
+# Stopwords
+stopwords_regex = paste(stopwords('en'), collapse = '\\b|\\b')
+stopwords_regex = paste0('\\b', stopwords_regex, '\\b')
+
+
+
+
+########### Clean data###############
+df_tmp <- df_train[1,]
+
+df_tmp_clean <- df_tmp %>%
+  mutate(comment_text = str_replace_all(comment_text, "[\r\n]", " ")) %>%
+  mutate(comment_text = tolower(comment_text)) %>%
+  mutate(comment_text = gsub("[[:punct:]]","",comment_text)) %>%
+  mutate(comment_text = str_replace_all(comment_text,stopwords_regex,""))
+
