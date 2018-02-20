b7f0515c5e28fad5804f0f4e2fa81555d31e229a
Patrick <Patrick@Patricks-MacBook-Pro.local>
2018-02-20 21:52:12 +0100

    Created a corpus clean version. Faster @ calc.

--- main.R
+++ main.R
@@ -19,32 +19,22 @@ stopwords_regex = paste(stopwords('en'), collapse = '\\b|\\b')
 stopwords_regex = paste0('\\b', stopwords_regex, '\\b')
 
 
+### Creating clean corpus ####
+
+comment_corpus <- Corpus(VectorSource(df_train$comment_text))
+
+corpus_clean <- tm_map(comment_corpus, tolower)
+corpus_clean <- tm_map(corpus_clean, removeNumbers)
+corpus_clean <- tm_map(corpus_clean, removeWords, stopwords())
+corpus_clean <- tm_map(corpus_clean, removePunctuation)
+corpus_clean <- tm_map(corpus_clean,stripWhitespace)
+corpus_clean <- tm_map(corpus_clean, stemDocument, language = "english")  
 
 
-########### Clean data###############
-df_tmp <- df_train[1:500,]
+#### Document Term Matrix ####
 
-df_tmp_clean <- df_tmp %>%
-  mutate(comment_text = str_replace_all(comment_text, "[\r\n]", " ")) %>%
-  mutate(comment_text = tolower(comment_text)) %>%
-  mutate(comment_text = gsub("[[:punct:]]","",comment_text)) %>%
-  mutate(comment_text = str_replace_all(comment_text,stopwords_regex,"")) %>%
-  mutate(comment_text = gsub("\\s+", " ", comment_text)) %>%
-  mutate(comment_text = removeNumbers(comment_text)) %>%
-  mutate(comment_text = stemDocument(comment_text))
-
-  
-#mutate(comment_text = text_tokens(comment_text, "en"))
-
-####### Create corpus ###############
-comment_text <- df_tmp_clean$comment_text
-comment_source <- VectorSource(comment_text)
-comment_corpus <- VCorpus(comment_source)
-
-comments_dtm <- DocumentTermMatrix(comment_corpus)
-comments_m <- as.matrix(comments_dtm)
-
-
+comments_dtm <- DocumentTermMatrix(corpus_clean)
+#comments_m <- as.matrix(comments_dtm)
 comments_m_sparse <- removeSparseTerms(comments_dtm,0.98)
 comments_m_sparse
 comments_m <- as.matrix(comments_m_sparse)
