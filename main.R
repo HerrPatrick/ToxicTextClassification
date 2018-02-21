ef8c27c9afa90d9f5b7ab6f70a9a2dd40fd96c04
Patrick <Patrick@Patricks-MacBook-Pro.local>
2018-02-21 23:00:44 +0100

    First model finished

--- main.R
+++ main.R
@@ -6,6 +6,9 @@ library(tm)
 library(SnowballC)
 #library(qdap) #problem with java re 
 library(e1071)
+library(gmodels)
+
+
 ### Load data################
 
 df_train <- read.csv("data/train.csv", sep = ",", stringsAsFactors = F)
@@ -19,7 +22,7 @@ for (i in 1:nrow(df_train)){
   df_train$class1[i] <- if(df_train$rowSum[i]==0) {'ham'} else {'spam'}
 }
 
-
+df_train$class1 <- as.factor(df_train$class1)
 
 ### Creating clean corpus ####
 
@@ -37,7 +40,7 @@ corpus_clean <- tm_map(corpus_clean, stemDocument, language = "english")
 
 comments_dtm <- DocumentTermMatrix(corpus_clean, list(dictionary = ))
 #comments_m <- as.matrix(comments_dtm)
-comments_m_sparse<- removeSparseTerms(comments_dtm,0.98)
+comments_m_sparse<- removeSparseTerms(comments_dtm,0.995)
 #comments_m_sparse
 comments_m <- as.matrix(comments_m_sparse)
 comments <- as.data.frame(comments_m)
@@ -49,7 +52,7 @@ comments <- as.data.frame(comments_m)
 i = ceiling(nrow(df_train)*0.7)
 
 df_train_model <- df_train[1:i, ]
-df_test_model <- df_train[i+1:nrow(df_train), ]
+df_test_model <- df_train[(i+1):nrow(df_train), ]
 
 
 
@@ -57,14 +60,16 @@ comments_train <- comments[1:i, ]
 comments_test <- comments[(i+1):nrow(df_train), ]
 
 comment_corpus_train <- corpus_clean[1:i]
-comment_corpus_test <- corpus_clean[i+1:nrow(df_train)]
+comment_corpus_test <- corpus_clean[(i+1):nrow(df_train)]
 prop.table(table(df_train_model$rowSum))
 prop.table(table(df_test_model$rowSum))
 
-comments_dict <- Terms(findFreqTerms(comments_train, 5)) 
+#comments_dict <- Terms(findFreqTerms(comments_train, 5)) 
 
 
 ##### Building model ########
 
 comment_classifier <- naiveBayes(comments_train, df_train_model$class1)
 comment_test_pred <- predict(comment_classifier, comments_test)
+
+CrossTable(comment_test_pred, df_test_model$class1, prop.chisq = FALSE, prop.t = FALSE, dnn = c('predicted','actual'))
