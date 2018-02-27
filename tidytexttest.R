57606224154ae6212772fff3b82b4814cf2efd97
Patrick Blomquist <PABL@statoil.com>
2018-02-27 08:40:29 +0100

    Added spam ham top identifier and plot

--- tidytexttest.R
+++ tidytexttest.R
@@ -15,25 +15,52 @@ library(ggplot2)
 
 df_train <- read.csv("data/train.csv", sep = ",", stringsAsFactors = F)
 
+
+
 data(stop_words)
 
 #### Data wrangle ##############
 
+df_train$rowSum <- rowSums(df_train[,3:8])
+df_train$classification1 <- ifelse(df_train$rowSum==0,'ham', 'spam')
+                                   
+
 text_df <- df_train %>%
-  select(id,comment_text) %>%
+  select(id,comment_text, classification1) %>%
   rename(word = comment_text) %>%
-  head %>%
-  unnest_tokens(word,word)
+  #mutate(word = str_extract(word, "[a-z']+")) %>%
+  head(10000) %>%
+  unnest_tokens(word,word) 
 
 
 tidy_comment <- text_df %>%
   anti_join(stop_words)
 
+
+######## Data exploration ############
 tidy_comment %>%
   count(word, sort = TRUE) %>%
+  filter(n>500) %>%
   mutate(word = reorder(word, n)) %>%
   ggplot(aes(word,n)) +
   geom_col() +
   xlab(NULL) +
   coord_flip()
+
+tidy_class <-tidy_comment %>%
+  count(word, classification1, sort = TRUE) %>%
+  group_by(classification1) %>%
+  top_n(10) %>%
+  ungroup() %>%
+  mutate(word = reorder(word,n))
+
+tidy_class %>% 
+  ggplot(aes(word, n, fill = classification1)) +
+  geom_col(show.legend = F) +
+  facet_wrap(~classification1, scales = "free_y") +
+  labs(y = "Contribution to classification", x = NULL) +
+  coord_flip()
+
+
+
 
